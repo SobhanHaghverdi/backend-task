f430c809c33f49ab002aae4dbae79cbe35235fea
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2025-12-07 14:02:09 +0330

    refactor: update swagger definition url and usage environment

--- src/config/index.js
+++ src/config/index.js
@@ -9,36 +9,25 @@ import corsConfig from "./cors-config.js";
 import helmetConfig from "./helmet-config.js";
 import swaggerConfig from "./swagger-config.js";
 import rateLimitConfig from "./rate-limit-config.js";
-import NodeEnv from "../common/constant/node-env-enum.js";
 
 function registerConfigs(app) {
   app.use(helmet(helmetConfig));
   app.use(cors(corsConfig));
   app.use(rateLimit(rateLimitConfig));
 
-  if (env.NODE_ENV === NodeEnv.DEVELOPMENT) {
-    const swagger = swaggerUi.setup(swaggerConfig, {
-      customSiteTitle: "Behtech Backend Task Swagger UI",
-    });
+  const swagger = swaggerUi.setup(swaggerConfig, {
+    customSiteTitle: "Behtech Backend Task Swagger UI",
+  });
 
-    app.use("/docs", swaggerUi.serve, swagger);
+  app.use("/", swaggerUi.serve, swagger);
 
-    app.get("/docs.json", (req, res) => {
-      res.setHeader("Content-Type", "application/json");
-      res.send(swaggerConfig);
-    });
-  }
+  app.get("/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerConfig);
+  });
 
   app.get("/health", (req, res) => {
     return res.status(200).json({ status: "OK", uptime: process.uptime() });
-  });
-
-  app.get("/version", (_, res) => {
-    return res.json({
-      app: "Behtech Backend Task",
-      version: "1.0.0",
-      environment: env.NODE_ENV,
-    });
   });
 }
 
